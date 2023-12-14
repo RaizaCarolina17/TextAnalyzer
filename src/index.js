44c8833f46a938b74bd88a802077d86ac1fd2ddb
Raiza Gatica <raizagatica@gmail.com>
2023-12-14 13:33:41 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+document.getElementById("reset-button").addEventListener("click", function () {
+    const textArea = document.querySelector('[name="user-input"]');
+    textArea.value = '';
+});
