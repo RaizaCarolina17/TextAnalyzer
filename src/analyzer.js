65959113267003e4de1e22f2116d7e1fa6f025ec
Raiza Gatica <raizagatica@gmail.com>
2024-01-04 13:24:43 -0300

    avanzado

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,54 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  
+  getWordCount: (cajadeTexto) => {
+    let palabras = 0;
+    const arrayWord = cajadeTexto.split(/\s+/);
+    for (let i = 0; i<arrayWord.length; i++){
+      if (arrayWord[i].trim() !== ''){
+        palabras++
+      }
+    }
+    return palabras;
   },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+  getCharacterCount: (cajadeTexto) => {
+    const numLetras = cajadeTexto.length;
+    return numLetras;
   },
-  getCharacterCountExcludingSpaces: (text) => {
+    
+  getCharacterCountExcludingSpaces: (cajadeTexto) => {
+    const caractEsp = cajadeTexto.replace(/\s+/g, '').replace(/[.,:;_?¡¿!"'()[\]{}«»]/g, '');
+    return caractEsp.length;
+    
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (cajadeTexto) => {
+    const palabras = cajadeTexto.trim().split(/\s+/).filter(word => word !== '');
+    let prmPalabras = 0;
+    for (let i = 0; i < palabras.length; i++) {
+      prmPalabras += palabras[i].length;
+    }
+    return palabras.length ? (prmPalabras / palabras.length).toFixed(2) : 0;
+    
+    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberCount: (text) => {
+  getNumberCount: (cajadeTexto) => {
+    const numeros = cajadeTexto.split(/\s+/).filter(word => /^\d+(\.\d+)?$/.test(word));
+    return numeros.length;
+   
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
+  getNumberSum: (cajadeTexto) => {
+    let sumaNumeros = 0;
+    for (let i = 0; i < cajadeTexto.length; i++) {
+      if (cajadeTexto[i] >= '0' && cajadeTexto[i] <= '9') {
+        sumaNumeros += parseInt(cajadeTexto[i], 10);
+      }
+    }
+    return sumaNumeros;
+  }
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+}
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const button= document.getElementById("reset-button");
+const wordCount = document.querySelector('li[data-testid="word-count"]');
+const charCount= document.querySelector("li[data-testid='character-count']");
+const noSpace= document.querySelector("li[data-testid='character-no-spaces-count']");
+const numCount= document.querySelector("li[data-testid='number-count']");
+const sumCount= document.querySelector("li[data-testid='number-sum']");
+const wordLength= document.querySelector("li[data-testid='word-length-average']");
+const cajaTexto= document.querySelector("textarea");
 
-document.getElementById("reset-button").addEventListener("click", function () {
-    const textArea = document.querySelector('[name="user-input"]');
-    textArea.value = '';
+
+
+cajaTexto.addEventListener('input', function(){
+  const cajadeTexto = cajaTexto.value
+  wordCount.innerHTML="Palabras:"+analyzer.getWordCount(cajadeTexto);
+  charCount.innerHTML="Caracteres:"+analyzer.getCharacterCount(cajadeTexto);
+  noSpace.innerHTML="Caracteres sin espacio:"+analyzer.getCharacterCountExcludingSpaces(cajadeTexto);
+  numCount.innerHTML="Números:"+analyzer.getNumberCount(cajadeTexto);
+  sumCount.innerHTML="Suma de números:"+analyzer.getNumberSum(cajadeTexto);
+  wordLength.innerHTML="Longitud de palabras:"+analyzer.getAverageWordLength(cajadeTexto);
+});
+
+button.addEventListener("click",function(){
+  cajaTexto.value=" ";
+  wordCount.innerHTML="Palabras: 0";
+  charCount.innerHTML="Caracteres: 0";
+  noSpace.innerHTML="Caracteres sin espacio: 0";
+  numCount.innerHTML="Números: 0";
+  sumCount.innerHTML="Suma de números: 0";
+  wordLength.innerHTML="Longitud de palabras: 0.00";
 });
